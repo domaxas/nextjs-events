4ad73298fa2bea2aa9500513b666aaabd880722a
Domas <katauskasdomas@gmail.com>
2022-12-14 13:18:20 +0200

    initial commit

---
+++ components/events/event-item.js
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import Button from "../ui/button";
+import DateIcon from "../icons/icons/date-icon";
+import AddressIcon from "../icons/icons/address-icon";
+import ArrowRightIcon from "../icons/icons/arrow-right-icon";
+import classes from "./event-item.module.css";
+
+function EventItem(props) {
+  const { title, image, date, location, id } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString("en-GB", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const formattedAddress = location.replace(", ", "\n");
+  const exporeLink = `/events/${id}`;
+
+  return (
+    <li className={classes.item}>
+      <img src={"/" + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <DateIcon />
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <AddressIcon />
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <Button link={exporeLink}>
+            <span>Explore Link</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default EventItem;
